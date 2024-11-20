d4fd4ac8907ee49ea577137358100ff5cb8cf75e
solsoleee <wsw3727@gmail.com>
2024-11-20 23:57:19 +0900

    #3 feat: model-back 연결

--- src/main/java/com/ssafy/home/service/RecommendationService.java
+++ src/main/java/com/ssafy/home/service/RecommendationService.java
@@ -15,18 +15,18 @@ public class RecommendationService {
 	private static final String FASTAPI_URL = "http://13.125.83.30:8000/recommend";
 
 	public List<Map<String, Object>> getRecommendations(int userId, String recommendationType) {
-		// 요청 데이터 생성
+
 		Map<String, Object> request = new HashMap<>();
+		//나중에 user_id는 로그인한 사용자가 들어가야 함
 		request.put("user_id", userId);
 		request.put("recommendation_type", recommendationType);
 
-		// HTTP 요청 생성
 		RestTemplate restTemplate = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(request, headers);
 
-		// FastAPI 호출 및 결과 반환
+		// FastAPI 호출 및 결과
 		try {
 			return restTemplate.postForObject(FASTAPI_URL, entity, List.class);
 		} catch (Exception e) {
