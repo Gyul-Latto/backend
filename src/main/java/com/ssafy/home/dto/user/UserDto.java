34402a425330f2b778a98ad0236878ac70a467b1
InGyu Moon <ansdlsrb1121@naver.com>
2024-11-21 18:01:56 +0900

    feat: UserDto 추가

--- src/main/java/com/ssafy/home/dto/user/UserDto.java
+++ src/main/java/com/ssafy/home/dto/user/UserDto.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
-	private long userId;
+	private Long userId;
 	private String username;
 	private String password;
 	private String email;
