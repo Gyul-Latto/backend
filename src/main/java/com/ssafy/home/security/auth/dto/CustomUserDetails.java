2eb90051e8e243cdb7623d49244d79a276ff3695
InGyu Moon <ansdlsrb1121@naver.com>
2024-11-21 17:22:44 +0900

    feat: CustomUserDetails, CustomUserDetailsService 구현

---
+++ src/main/java/com/ssafy/home/security/auth/dto/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.ssafy.home.security.auth.dto;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.ssafy.home.dto.user.UserDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+	private final UserDto userDto;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	public long getUserId() {
+		return userDto.getUserId();
+	}
+
+	@Override
+	public String getPassword() {
+		return userDto.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return userDto.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return UserDetails.super.isAccountNonExpired();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return UserDetails.super.isAccountNonLocked();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return UserDetails.super.isCredentialsNonExpired();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return UserDetails.super.isEnabled();
+	}
+}
