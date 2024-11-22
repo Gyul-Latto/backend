1d38726d2b7f01f8b80eb22d3554bd238cfc2664
InGyu Moon <ansdlsrb1121@naver.com>
2024-11-22 13:50:36 +0900

    feat: 회원가입, 정보 조회, 수정, 탈퇴 구현

--- src/main/java/com/ssafy/home/controller/UserController.java
+++ src/main/java/com/ssafy/home/controller/UserController.java
@@ -1,25 +1,62 @@
 package com.ssafy.home.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.home.common.response.SuccessResponse;
 import com.ssafy.home.dto.user.SignUpRequestDto;
+import com.ssafy.home.dto.user.UserInfoDto;
+import com.ssafy.home.dto.user.UserUpdateDto;
+import com.ssafy.home.security.auth.dto.CustomUserDetails;
 import com.ssafy.home.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/v1/members")
+@RequestMapping("/api/members")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
 	private final UserService userService;
 
+	// 회원가입
 	@PostMapping
 	public SuccessResponse<Boolean> createUser(@RequestBody SignUpRequestDto signUpRequestDto) {
 		return SuccessResponse.of(userService.insertUser(signUpRequestDto));
 	}
+
+	// 회원조회
+	@GetMapping("/{userId}")
+	public SuccessResponse<UserInfoDto> getUsers(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable("userId") Long urlUserId
+	) {
+		return SuccessResponse.of(userService.getUsers(userDetails.getUserId(), urlUserId));
+	}
+
+	// 회원수정
+	@PutMapping("/{userId}")
+	public SuccessResponse<Boolean> updateUsers(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable("userId") Long urlUserId,
+		@RequestBody UserUpdateDto userUpdateDto
+	) {
+		return SuccessResponse.of(userService.updateUsers(userDetails.getUserId(), urlUserId, userUpdateDto));
+	}
+
+	// 회원탈퇴
+	@DeleteMapping("/{userId}")
+	public SuccessResponse<Boolean> deleteUsers(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable("userId") Long urlUserId
+	) {
+		return SuccessResponse.of(userService.deleteUsers(userDetails.getUserId(), urlUserId));
+	}
+
 }
--- src/main/java/com/ssafy/home/service/UserService.java
+++ src/main/java/com/ssafy/home/service/UserService.java
@@ -1,13 +1,13 @@
 package com.ssafy.home.service;
 
-import java.util.Optional;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.home.common.exception.exceptions.BadRequestException;
 import com.ssafy.home.common.response.ErrorCode;
 import com.ssafy.home.dto.user.SignUpRequestDto;
-import com.ssafy.home.dto.user.UserDto;
+import com.ssafy.home.dto.user.UserInfoDto;
+import com.ssafy.home.dto.user.UserUpdateDto;
 import com.ssafy.home.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,14 +18,66 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserService {
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	public boolean insertUser(SignUpRequestDto signUpRequestDto) {
-		Optional<UserDto> user = userRepository.findByEmail(signUpRequestDto.getEmail());
-		// User가 존재하면 BadRequestException 발생
-		if (user.isPresent()) {
-			throw new BadRequestException(ErrorCode.DUPLICATE_EMAIL);
+	// 회원조회
+	public UserInfoDto getUsers(Long tokenUserId, Long urlUserId) {
+		// tokenUserId와 urlUserId가 다르면 Exception 발생
+		if (!tokenUserId.equals(urlUserId)) {
+			throw new BadRequestException(ErrorCode.FORBIDDEN);
 		}
+		// urlUserId로 사용자 조회, 없으면 Exception 발생, 있으면 UserInfoDto로 변환해서 반환
+		return userRepository.findByUserId(urlUserId).map(user -> UserInfoDto.builder()
+			.userId(user.getUserId())
+			.email(user.getEmail())
+			.username(user.getUsername())
+			.birthday(user.getBirthday())
+			.gender(user.getGender())
+			.build()
+		).orElseThrow(() -> new BadRequestException(ErrorCode.USER_NOT_FOUND));
+	}
+
+	// 회원가입
+	public boolean insertUser(SignUpRequestDto signUpRequestDto) {
+
+		// 이메일 중복 체크
+		userRepository.findByEmail(signUpRequestDto.getEmail())
+			.ifPresent(user -> {
+				throw new BadRequestException(ErrorCode.DUPLICATE_EMAIL);
+			});
+
+		// 비밀번호 암호화
+		signUpRequestDto.setPassword(bCryptPasswordEncoder.encode(signUpRequestDto.getPassword()));
+
 		return userRepository.insertUser(signUpRequestDto);
 	}
 
+	// 회원수정
+	public boolean updateUsers(Long tokenUserId, Long urlUserId, UserUpdateDto userUpdateDto) {
+		// tokenUserId와 urlUserId가 다르면 Exception 발생
+		if (!tokenUserId.equals(urlUserId)) {
+			throw new BadRequestException(ErrorCode.FORBIDDEN);
+		}
+
+		// urlUserId로 사용자 조회, 없으면 Exception 발생
+		userRepository.findByUserId(urlUserId).orElseThrow(() -> new BadRequestException(ErrorCode.USER_NOT_FOUND));
+
+		// userId 설정
+		userUpdateDto.setUserId(tokenUserId);
+		// 비밀번호 암호화
+		userUpdateDto.setPassword(bCryptPasswordEncoder.encode(userUpdateDto.getPassword()));
+
+		// 사용자 정보 수정 성공하면 true 반환
+		return userRepository.updateUser(userUpdateDto) > 0;
+	}
+
+	// 회원탈퇴
+	public boolean deleteUsers(Long tokenUserId, Long urlUserId) {
+		// tokenUserId와 urlUserId가 다르면 Exception 발생
+		if (!tokenUserId.equals(urlUserId)) {
+			throw new BadRequestException(ErrorCode.FORBIDDEN);
+		}
+		// urlUserId로 사용자 삭제 성공하면 true 반환
+		return userRepository.deleteUser(urlUserId) > 0;
+	}
 }
